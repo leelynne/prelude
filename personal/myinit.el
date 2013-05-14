9f9626a46806c165e710fae2dedcd8780044eb31
leelynne <leelynne@gmail.com>
2013-05-14 09:25:45 -0700

    Add some custom key bindings.

--- personal/myinit.el
+++ personal/myinit.el
@@ -1,1 +1,21 @@
+;;; myinit.el --- my crap
+;;; Commentary:
+;;
+;;; Code:
+
+(setq-default tab-width 4)
 (global-set-key "\C-w" 'backward-kill-word)
+
+;; Tab stuff
+(setq c-basic-offset 4) ; indents 4 chars
+(setq tab-width 4)          ; and 4 char wide for TAB
+(setq-default indent-tabs-mode nil) ; And force use of spaces
+
+;; Add F12 to toggle line wrap
+(global-set-key [f12] 'toggle-truncate-lines)
+;; Read only prompt
+(add-hook 'shell-mode-hook
+          '(lambda () (toggle-truncate-lines 1)))
+(setq comint-prompt-read-only t)
+
+;;; myinit.el ends here
