e343b52ffb904859e8dc9cf5d4a2a2f332d11ed7
leelynne <leelynne@gmail.com>
2013-05-15 17:02:37 -0700

    Add a few bindings.

--- personal/myinit.el
+++ personal/myinit.el
@@ -13,6 +13,12 @@
 (setq ecb-tip-of-the-day nil)
 (setq-default tab-width 4)
 (global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+
+(global-set-key (kbd "C-c <C-left>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-c <C-right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-c <C-down>") 'shrink-window)
+(global-set-key (kbd "C-c <C-up>") 'enlarge-window)
 
 ;; Tab stuff
 (setq c-basic-offset 4) ; indents 4 chars
